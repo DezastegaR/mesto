938e1835cb32dec5ede89e3eeb5a62e51b7b9593
Pavel Pavlyuchkov <dezastegar94@gmail.com>
2022-08-11 13:16:51 +0300

    feat: js

---
+++ script/main.js
@@ -0,0 +1,13 @@
+let popup = document.querySelector('.popup');
+
+let editProfileButton = document.querySelector('.profile__edit-button');
+
+let closeProfileButton = document.querySelector('.popup__close-button');
+let formSaveButton = document.querySelector('.form__save-button');
+
+let profileName = document.querySelector('.profile__name');
+let profileRole = document.querySelector('.profile__role');
+
+let formElement = document.querySelector('.form')
+let inputName = formElement.querySelector('.form__input_type_name');
+let inputRole = formElement.querySelector('.form__input_type_role');
